e8c89bbd4f2d545ac7420db6b6faf327c2f6f13c
mantseakai <mantseakai@gmail.com>
2021-08-31 19:12:36 +0000

    Update singly_linked_list_exercise.py
    With the two additional methods for the exercise remove_by_value and insert_after_value, I called the insert_at and remove_at functions.

--- data_structures/3_LinkedList/Solution/singly_linked_list_exercise.py
+++ data_structures/3_LinkedList/Solution/singly_linked_list_exercise.py
@@ -11,11 +11,10 @@ class LinkedList:
         if self.head is None:
             print("Linked list is empty")
             return
-
         itr = self.head
         llstr = ''
         while itr:
-            llstr += str(itr.data) + ' --> '
+            llstr += str(itr.data)+' --> ' if itr.next else str(itr.data)
             itr = itr.next
         print(llstr)
 
@@ -86,53 +85,49 @@ class LinkedList:
         for data in data_list:
             self.insert_at_end(data)
 
+    def insert_after_value(self,data_after, data_to_insert):
+    	if self.head is None:
+    		return
 
-    def insert_after_value(self, data_after, data_to_insert):
-        if self.head is None:
-            return
-
-        if self.head.data==data_after:
-            self.head.next = Node(data_to_insert,self.head.next)
-            return
-
-        itr = self.head
-        while itr:
-            if itr.data == data_after:
-                itr.next = Node(data_to_insert, itr.next)
-                break
-
-            itr = itr.next
+    	count = 0
+    	itr = self.head
+    	while itr:
+    		if itr.data == data_after:
+    			self.insert_at(count+1,data_to_insert)
+    			break
+    		itr = itr.next
+    		count+=1
 
     def remove_by_value(self, data):
-        if self.head is None:
-            return
+    	if self.head is None:
+    		return
 
-        if self.head.data == data:
-            self.head = self.head.next
-            return
+    	count = 0
+    	itr = self.head
 
-        itr = self.head
-        while itr.next:
-            if itr.next.data == data:
-                itr.next = itr.next.next
-                break
-            itr = itr.next
+    	while itr:
+    		if itr.data == data:
+    			self.remove_at(count)
+    		itr = itr.next
+    		count+=1
+
+
 
 if __name__ == '__main__':
-    ll = LinkedList()
-    ll.insert_values(["banana","mango","grapes","orange"])
-    ll.print()
-    ll.insert_after_value("mango","apple")
-    ll.print()
-    ll.remove_by_value("orange")
-    ll.print()
-    ll.remove_by_value("figs")
-    ll.print()
-    ll.remove_by_value("banana")
-    ll.remove_by_value("mango")
-    ll.remove_by_value("apple")
-    ll.remove_by_value("grapes")
-    ll.print()
+	ll = LinkedList()
+	ll.insert_values(["banana","mango","grapes","orange"])
+	ll.print()
+	ll.insert_after_value("mango","apple") # insert apple after mango
+	ll.print()
+	ll.remove_by_value("orange") # remove orange from linked list
+	ll.print()
+	ll.remove_by_value("figs")
+	ll.print()
+	ll.remove_by_value("banana")
+	ll.remove_by_value("mango")
+	ll.remove_by_value("apple")
+	ll.remove_by_value("grapes")
+	ll.print()
 
     # ll.insert_values([45,7,12,567,99])
     # ll.insert_at_end(67)
